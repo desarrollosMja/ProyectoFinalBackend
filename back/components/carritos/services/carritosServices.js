ba222518156154ffd09ff0f6450dc30ce7f8c670
Desarrollos MJA <desarrollos.mja@gmail.com>
2022-04-22 11:26:07 -0300

    segunda entrega

--- back/components/carritos/services/carritosServices.js
+++ back/components/carritos/services/carritosServices.js
@@ -1,129 +1,79 @@
-//let { crearCarrito, traerCarrito, updateCarrito, borrarCarrito } = require("./crudFirebase")
-const ManejadorCarritos = require("../../../utils/fileSystem/fsCarritos")
-const manejador = new ManejadorCarritos("carritos.txt")
+let { crearCarrito, traerCarrito, updateCarrito, borrarCarrito } = require("./crudFirebase")
 
 class CarritosServices{
 
     async createCarrito(data){
-        const cartId = await manejador.createCart(data)
-        return cartId
-        /*Volver a usar cuando retorne a DB*/
-        // try {
-        //     const carrito = {
-        //         item: []
-        //     }
-        //     carrito.item.push(data.item)
-        //     const idCarritoNuevo = await crearCarrito(carrito)
-        //     return idCarritoNuevo
-        // } catch (error) {
-        //     return error;
-        // }
+        try {
+            const carrito = {
+                item: []
+            }
+            carrito.item.push(data.item)
+            const idCarritoNuevo = await crearCarrito(carrito)
+            return idCarritoNuevo
+        } catch (error) {
+            return error;
+        }
     }
 
     async addProducto(req){
-        const carritos = await manejador.getCart()
-        for (const carrito of carritos) {
-            if (carrito.id == req.params.cid){
-                carrito.items.push(req.body.item)
-            }
+        try {
+            const carrito = await this.getCarrito(req)
+            carrito.item.push(req.body.item)
+            await updateCarrito(req.params.cid, carrito)
+        } catch (error) {
+            return error;
         }
-        await manejador.updateCart(carritos)
-
-        /*Volver a usar cuando retorne a DB*/
-        // try {
-        //     const carrito = await this.getCarrito(req)
-        //     carrito.item.push(req.body.item)
-        //     await updateCarrito(req.params.id, carrito)
-        // } catch (error) {
-        //     return error;
-        // }
     }
 
     async addUnidad(req){
-        const carritos = await manejador.getCart()
-        for (const carrito of carritos) {
-            if (carrito.id == req.params.cid){
-                carrito.items.forEach(item => {
-                    if (item.id == req.params.pid){
-                        item.addedToCart++
-                    }
-                })
+        try {
+            const carrito = await this.getCarrito({params: {cid: req.params.cid}})
+            for (let i = 0; i < carrito.item.length; i++){
+                if (carrito.item[i]._id == req.params.pid) {
+                    carrito.item[i].addedToCart += 1
+                }
             }
+            await updateCarrito(req.params.cid, carrito)
+        } catch (error) {
+            return error;
         }
-        await manejador.updateCart(carritos)
-
-        /*Volver a usar cuando retorne a DB*/
-        // try {
-        //     const carrito = await this.getCarrito({params: {id: req.params.idCarrito}})
-        //     for (let i = 0; i < carrito.item.length; i++){
-        //         /*Cuando vuelva a usar DB cambiar "id" por "_id"*/
-        //         if (carrito.item[i].id == req.params.idProducto) {
-        //             carrito.item[i].addedToCart += 1
-        //         }
-        //     }
-        //     await updateCarrito(req.params.idCarrito, carrito)
-        // } catch (error) {
-        //     return error;
-        // }
     }
 
     async deleteProducto(req){
-        const carritos = await manejador.getCart()
-        for (const carrito of carritos) {
-            for (let i = 0; i < carrito.items.length; i++){
-                if (carrito.items[i].id == req.params.pid) {
-                    if (carrito.items.length == 1){
-                        await manejador.deleteCart(req.params.cid)
+        try {
+            const carrito = await this.getCarrito({params: {cid: req.params.cid}})
+            for (let i = 0; i < carrito.item.length; i++){
+                if (carrito.item[i]._id == req.params.pid) {
+                    if (carrito.item.length == 1){
+                        await borrarCarrito(req.params.cid)
                     } else{
-                        carrito.items.splice(i, 1);
-                        await manejador.updateCart(carritos)
+                        carrito.item.splice(i, 1);
+                        await updateCarrito(req.params.cid, carrito)
                     }
                 }
             }
+        } catch (error) {
+            return error;
         }
-
-        /*Volver a usar cuando retorne a DB*/
-        // try {
-        //     const carrito = await this.getCarrito({params: {id: req.params.idCarrito}})
-        //     for (let i = 0; i < carrito.item.length; i++){
-        //         /*Cuando vuelva a usar DB cambiar "id" por "_id"*/
-        //         if (carrito.item[i].id == req.params.idProducto) {
-        //             if (carrito.item.length == 1){
-        //                 await borrarCarrito(req.params.idCarrito)
-        //             } else{
-        //                 carrito.item.splice(i, 1);
-        //                 await updateCarrito(req.params.idCarrito, carrito)
-        //             }
-        //         }
-        //     }
-        // } catch (error) {
-        //     return error;
-        // }
     }
 
     async deleteCarrito(req){
-        await manejador.deleteCart(req.params.cid)
-        /*Volver a usar cuando retorne a DB*/
-        // try {
-        //     await borrarCarrito(req.params.id)
-        // } catch (error) {
-        //     console.log("error:", error)
-        //     return error;
-        // }
+        try {
+            await borrarCarrito(req.params.cid)
+        } catch (error) {
+            console.log("error:", error)
+            return error;
+        }
     }
 
     async getCarrito(req){
-        const carritos = await manejador.getCart()
-        const carritoEncontrado = carritos.find(elem => elem.id == req.params.cid)
-        return carritoEncontrado
-        /*Volver a usar cuando retorne a DB*/
-        // try {
-        //     const carrito = await traerCarrito(req.params.id);
-        //     const carritoEncontrado = carrito.data()
-        //     return carritoEncontrado
-        // } catch (error) {
-        //     return error;
-        // }
+        try {
+            const carrito = await traerCarrito(req.params.cid);
+            const carritoEncontrado = carrito.data()
+            return carritoEncontrado
+        } catch (error) {
+            return error;
+        }
     }
 }
 
--- back/components/productos/services/productosService.js
+++ back/components/productos/services/productosService.js
@@ -1,7 +1,5 @@
 const Producto = require("../productos")
-//const { crearProducto, modificarProducto, obtenerProductos, obtenerProductoPorID, borrarProducto } = require("../model/productosModel")
-const ManejadorProductos = require("../../../utils/fileSystem/fsService")
-const manejadorArhcivos = new ManejadorProductos("productos.txt")
+const { crearProducto, modificarProducto, obtenerProductos, obtenerProductoPorID, borrarProducto } = require("../model/productosModel")
 
 class ProductosServices{
     constructor(){
@@ -11,87 +9,49 @@ class ProductosServices{
     async getProductos(req){
         try {
             if (!req.params.pid) {
-                this.productos = await manejadorArhcivos.getAll()
+                this.productos = await obtenerProductos()
                 return this.productos
             } else {
-                const producto = await manejadorArhcivos.getById(req.params.pid)
-                return producto
+                const producto = await obtenerProductoPorID(req.params.pid)
+                if (producto == false){
+                    return {error: "El id ingresado no corresponde a un producto"}
+                } else return producto
             }
         } catch (error) {
             return {error: error}
         }
-        /*Para ser usado sólo con DB*/
-        // try {
-        //     if (!req.params.id) {
-        //         this.productos = await obtenerProductos()
-        //         return this.productos
-        //     } else {
-        //         const producto = await obtenerProductoPorID(req.params.id)
-        //         if (producto == false){
-        //             return {error: "El id ingresado no corresponde a un producto"}
-        //         } else return producto
-        //     }
-        // } catch (error) {
-        //     return {error: error}
-        // }
     }
 
     async createProducto(req){
         try {
             let {nombre, descripcion, codigo, urlFoto, precio, stock} = req.body
             const nuevoProducto = new Producto(nombre, descripcion, codigo, urlFoto, precio, stock)
-            await manejadorArhcivos.create(nuevoProducto)
-            const productos = await manejadorArhcivos.getAll()
+            await crearProducto(nuevoProducto)
+            const productos = await obtenerProductos()
             return productos
         } catch (error) {
             return {error: error}
         }
-        /*Para ser usado sólo con DB*/
-        // try {
-        //     let {nombre, descripcion, codigo, urlFoto, precio, stock} = req.body
-        //     const nuevoProducto = new Producto(nombre, descripcion, codigo, urlFoto, precio, stock)
-        //     await crearProducto(nuevoProducto)
-        //     const productos = await obtenerProductos()
-        //     return productos
-        // } catch (error) {
-        //     return {error: error}
-        // }
     }
 
     async deleteProducto(req){
         try {
-            await manejadorArhcivos.delete(req.params.pid)
-            const productos = await manejadorArhcivos.getAll()
+            await borrarProducto(req.params.pid)
+            const productos = await obtenerProductos()
             return productos
         } catch (error) {
             return {error: error}
         }
-        /*Para ser usado sólo con DB*/
-        // try {
-        //     await borrarProducto(req.params.id)
-        //     const productos = await obtenerProductos()
-        //     return productos
-        // } catch (error) {
-        //     return {error: error}
-        // }
     }
 
     async modifyProducto(req){
         try {
-            await manejadorArhcivos.modify(req.body._id, req.body)
-            const productos = await manejadorArhcivos.getAll()
+            await modificarProducto(req.body._id, req.body)
+            const productos = await obtenerProductos()
             return productos
         } catch (error) {
             return {error: error}
         }
-        /*Para ser usado sólo con DB*/
-        // try {
-        //     await modificarProducto(req.body._id, req.body)
-        //     const productos = await obtenerProductos()
-        //     return productos
-        // } catch (error) {
-        //     return {error: error}
-        // }
     }
 }
 
